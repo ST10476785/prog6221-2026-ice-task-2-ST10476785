d88ecefbc6b19a44938856bf30e8e423f5e5998f
ST10476785 <ST10476785@myemeris.edu.za>
2026-03-30 15:01:43 +0200

    Added step 1 (Ship command method )
    Added automatic properties and command execute method with speechSynthesizer  to read out print lines

--- Program.cs
+++ Program.cs
@@ -1,10 +1,35 @@
-﻿namespace ICE_TASK_2
+﻿
+using System.Speech.Synthesis;
+namespace ICE_TASK_2
 {
     internal class Program
     {
+
+
+        public class ShipCommand {
+            // Step 1 ship command class automatic properties (getters and setters) and executeCommand method
+
+            public String InputText { get; set; }
+            public String AuthorizedUser { get; set; }
+
+
+
+
+            public virtual void ExecuteCommand() {
+                Console.WriteLine("Processing Command");
+
+                SpeechSynthesizer speechSynthesizer = new SpeechSynthesizer();
+
+                speechSynthesizer.Speak("Command receieved");
+
+            }
+
+
+
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+      
         }
     }
 }
