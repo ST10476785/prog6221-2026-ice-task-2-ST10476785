e2ccb6c6836342b079aaa837bdbba86de44f9c1d
ST10476785 <ST10476785@myemeris.edu.za>
2026-03-30 16:32:32 +0200

    Added output print lines

--- Program.cs
+++ Program.cs
@@ -65,7 +65,54 @@ namespace ICE_TASK_2
         
         static void Main(string[] args)
         {
-      
+
+            //Step 3 Main method to prompt user input for rank, name and the command that will be spoken by the speechsynthesizer
+            Console.Write("Enter your rank and name (e.g., Captain Miller: ");
+            var userName = Console.ReadLine();
+
+            Console.Write("Enter your command: ");
+            var commandText = Console.ReadLine();
+
+            // String Methods for LogCode
+            string formattedName = userName.Trim().Replace(" ", "").ToUpper();
+            string formattedCommand = commandText.Trim().Replace(" ", "").ToUpper();
+
+            var logCode = formattedName + "-" + formattedCommand;
+
+            // Handling Nullables
+            Console.Write("Enter Alert Urgency (1 to 10) or leave empty for Standard Mode: ");
+            string urgencyInput = Console.ReadLine();
+
+            int? pitchLevel = null;
+
+            if (!string.IsNullOrWhiteSpace(urgencyInput) && int.TryParse(urgencyInput, out int parsedPitch))
+            {
+                pitchLevel = parsedPitch; 
+            }
+
+
+
+            Console.WriteLine("LOG CODE: " + logCode);
+
+
+            //  child class objecrt to execute the command with the user input and pitch level
+            VocalResponse currentCommand = new VocalResponse
+            {
+                AuthorizedUser = userName,
+                InputText = commandText,
+                AlertPitch = pitchLevel
+            };
+
+            // Call overridden method
+            currentCommand.ExecuteCommand();
+
+            Console.WriteLine("Press any key to close the terminal...");
+            Console.ReadKey();
+
+
+
+
+
         }
     }
 }
--- obj/Debug/net10.0/ICE TASK 2.AssemblyInfo.cs
+++ obj/Debug/net10.0/ICE TASK 2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ICE TASK 2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1bbf8cf1591e447435e7c971d141ca0bf097f9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ICE TASK 2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ICE TASK 2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
